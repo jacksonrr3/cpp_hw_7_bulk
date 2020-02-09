146df3406b4392b14bcba4eb53c9b0da2f2805b7
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2020-02-09 18:13:09 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,12 @@ int main(int, char * argv[]) {
 	Command command_log(std::atoi(argv[1]));
 	command_log.subscribe(std::make_unique<FileObserver>());
 	command_log.subscribe(std::make_unique<TerminalObserver>());
-	command_log.add_command();
+	//command_log.add_command();
+	std::string s;
+	while (std::getline(std::cin, s) && !std::cin.eof()){
+		command_log.add_command(s);
+	}
+	
 
 	return 0;
 }
